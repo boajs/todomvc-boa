9fde82ae32eff2f9bbbc4a87e6c22da1c399729d
bouzuya <m@bouzuya.net>
2016-03-20 23:21:10 +0900

    add todo behaviors

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,40 @@ const id = (): string => {
   return String(new Date().getTime());
 };
 
+// actions/
+
+const makeAction = <T>(type: string) => {
+  const create = (data: T) => ({ type, data });
+  const filter = (action$: O<A<any>>): O<T> => {
+    return action$
+      .filter(action => action.type === type)
+      .map(({ data }) => data);
+  };
+  return { create, filter };
+};
+
+const {
+  create: render
+} = makeAction<State>('render');
+
+// actions/props/todo/
+
+const {
+  create: propsTodoChange,
+  filter: propsTodoChange$
+} = makeAction<string>('props/todo/change');
+
+// actions/views/
+
+const {
+  create: viewsNewTodoChange,
+  filter: viewsNewTodoChange$
+} = makeAction<Event>('views/new-todo/change');
+const {
+  create: viewsNewTodoKeyup,
+  filter: viewsNewTodoKeyup$
+} = makeAction<KeyboardEvent>('views/new-todo/keyup');
+
 // views/
 
 const headerView = (state: State, helpers: any): any => {
@@ -29,7 +63,10 @@ const headerView = (state: State, helpers: any): any => {
     h('h1', ['todos']),
     h('input.new-todo', {
       placeholder: 'What needs to be done?',
-      autofocus: true
+      autofocus: true,
+      onchange: data => e(viewsNewTodoChange(data)),
+      onkeyup: data => e(viewsNewTodoKeyup(data)),
+      value: state.todo
     })
   ]);
 };
@@ -53,16 +90,41 @@ const view = (state: State, helpers: any): any => {
 
 // props/
 
-const props = (action$: O<A<any>>, options: any): O<A<any>> => {
+// props/todo
+
+const todo$ = (action$: O<A<any>>, state: string): O<string> => {
+  const todoUpdater$ = propsTodoChange$(action$).map(value => () => value);
+  const todo$: O<string> = O
+    .of(state)
+    .merge(todoUpdater$)
+    .scan((state, updater) => updater(state));
+  return todo$;
+};
+
+// props/index
+
+const props = (action$: O<A<any>>, options: any): O<State> => {
   const { state }: { state: State; } = options;
-  return O.empty();
+  return O.combineLatest(
+    todo$(action$, state.todo),
+    (todo) => Object.assign({}, state, { todo })
+  )
+    .do(console.log.bind(console)) // TODO: state logger for debug
+    .publishReplay(1)
+    .refCount();
 };
 
 // maps/
 
 const maps = (action$: O<A<any>>, options: any): O<A<any>> => {
   const { state$ }: { state$: O<State> } = options;
-  return O.empty();
+  return O.merge(
+    viewsNewTodoChange$(action$)
+      .map(({ target }) => (<any>target).value)
+      .map(propsTodoChange),
+    state$
+      .map(render)
+  );
 };
 
 // app
