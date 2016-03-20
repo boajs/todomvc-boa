9add088dd478f3667b5d06ae422956c32fa1ae6c
bouzuya <m@bouzuya.net>
2016-03-20 19:50:11 +0900

    add initial implementation

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { run, O, A } from 'b-o-a';
+import { init as dom } from 'b-o-a/handlers/dom';
+
+// views
+
+const view = (state: any, helpers: any): any => {
+  const { create: h } = helpers;
+  return h('div');
+};
+
+// app
+
+const handler = (action$: O<A<any>>, options: any): O<A<any>> => {
+  return O.empty();
+};
+
+// client
+
+const main = () => {
+  run((action$, options) => {
+    const log$ = action$
+      .do(console.log.bind(console)) // action logger for debug
+      .share();
+    const dom$ = dom({
+      render: view,
+      root: '.todoapp'
+    }).handler(log$, options);
+    return handler(dom$, options);
+  });
+};
+
+main();
