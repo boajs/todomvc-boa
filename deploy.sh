889d043bf335cf4ce1bf7c93519348afcb7499d7
bouzuya <m@bouzuya.net>
2016-03-21 01:30:16 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-git checkout gh-pages
+get fetch origin gh-pages:gh-pages
+git checkout origin/gh-pages
 git add index.html
 git add js/app.js
 git add node_modules/todomvc-app-css/index.css
@@ -9,5 +10,5 @@ git add node_modules/todomvc-common/base.js
 git config --global user.name 'bouzuya'
 git config --global user.email 'm@bouzuya.net'
 git commit -m "$(date +'%Y-%m-%dT%H:%M:%S%z')"
-git push origin gh-pages
+git push origin gh-pages:gh-pages
 
